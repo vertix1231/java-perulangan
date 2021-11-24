a19b37f9ab24cd0edab785a5a9a77dd327793578
Irsyad Muhammad Andre <irsyad_vertix@apps.ipb.ac.id>
2021-11-24 11:34:54 +0700

    test14

--- src/com/sqa/hellojuara/lagione/ContohPerulangan.java
+++ src/com/sqa/hellojuara/lagione/ContohPerulangan.java
@@ -6,7 +6,7 @@ public class ContohPerulangan {
 		// TODO Auto-generated method stub
 		int n = 8;
 		for(int i=0;i<5;i++) {
-			System.out.println("*****");
+			System.out.println("*****n");
 			System.out.printf("%d x %d = %d %n",n,i,n*i);
 		}
 
